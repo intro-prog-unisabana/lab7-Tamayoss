d25a09237047c6258af5c627cc1d54a45e04ed1a
Emilio Tamayo <emiliotase@unisabana.edu.co>
2026-04-16 10:17:43 -0500

    1

--- password_manager.py
+++ password_manager.py
@@ -4,17 +4,34 @@ from caesar import caesar_encrypt
 
 
 def encrypt_single_pass(filename: str) -> None:
-    """TODO: Parte 1."""
-    pass
+    with open(filename, "r") as f:
+        password = f.read().strip()
+    encrypted_passwords = caesar_encrypt(password)
+
+    with open(filename, "w") as f:
+        f.write(encrypted_passwords)
+
+    if __name__ == "__name__":
+        encrypt_single_pass("password.txt")
+    pass  
 
 
 def encrypt_passwords_in_file(filename: str) -> None:
-    """TODO: Parte 2."""
+    with open(filename, "r") as f:
+        reader = csv.reader(f)
+        rows = [row for row in reader] 
+    
+    for row in rows:
+        row[2] = caesar_encrypt(row[2])
+    with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(rows)
     pass
 
-
 def change_password(filename: str, website: str, password: str) -> bool:
-    """TODO: Parte 3."""
+    with open(filename, "w", newline="") as f:
+        reader = csv.reader(f)
+        rows = list(reader)
     pass
 
 
